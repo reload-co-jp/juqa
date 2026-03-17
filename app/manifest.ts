149c9d809c5ba856dba2654d55ade65d6fd6ec3c
kixixixixi <kixixixixi@gmail.com>
2026-03-17 20:40:22 +0900

    Add TWA

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,10 +15,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#7cbe8c",
     icons: [
       {
-        src: basePath + "/icons/icon.svg",
-        sizes: "any",
-        type: "image/svg+xml",
+        src: basePath + "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
         purpose: "any",
+      },
+      {
+        src: basePath + "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
       },
     ],
   }
