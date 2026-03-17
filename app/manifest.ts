4f4820a5dc65b625b3787580016459c1cc957db3
kixixixixi <kixixixixi@gmail.com>
2026-03-17 20:50:47 +0900

    Fix for build

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next"
 
+export const dynamic = "force-static"
+
 const basePath = process.env.NODE_ENV === "production" ? "/juqa" : ""
 
 export default function manifest(): MetadataRoute.Manifest {
