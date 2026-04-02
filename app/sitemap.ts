e3eb2956e08a56322f85bd4837f9294629779290
kixixixixi <kixixixixi@gmail.com>
2026-04-02 16:10:49 +0900

    Fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next"
 import { plants, families } from "lib/data"
 
+export const dynamic = "force-static"
+
 const siteUrl = "https://reload-co-jp.github.io/juqa"
 
 export default function sitemap(): MetadataRoute.Sitemap {
